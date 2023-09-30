7ca6682b51ee6e91001ca3bff6a521822da89b38
IvanGerm <germanivanomsk@gmail.com>
2023-10-01 02:05:05 +0300

    commit-1

---
+++ cpp/middle_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int num1, num2, middle_value;
+
+int main()
+{
+	cout<<"Введите число: "<<endl;
+	cin>>num1>>num2;
+	middle_value=(num1+num2)/2;
+	cout<<"Среднее арифмитическое "<<num1<<" и "<<num2<<" = "<<middle_value;
+
+	return 0;
+}
