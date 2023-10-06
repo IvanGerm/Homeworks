a005d4ce99fdbcd97764eb7b39e3887bf1e169e6
IvanGerm <germanivanomsk@gmail.com>
2023-10-06 21:31:06 +0300

    commit-4

---
+++ Python/square-13-10-23.py
@@ -0,0 +1,6 @@
+print("Введите длину и ширину: ")
+length, width = float(input()), float(input());
+square = length * width
+perimetr = 2 * (length + width)
+print("Площадь:", square)
+print("Периметр:", perimetr)
